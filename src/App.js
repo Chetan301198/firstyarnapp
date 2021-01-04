5a77bd19e8a49c2610e43e96f0f05336e24b84fe
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-01-04 13:47:10 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Card from "./Components/Cards/Cards";
-import green from "../Assets/images/nike1.png";
-import red from "../Assets/images/nike2.png";
-import blue from "../Assets/images/nike3.png";
+import green from "../src/Assets/images/nike1.png";
+import red from "../src/Assets/images/nike2.png";
+import blue from "../src/Assets/images/nike3.png";
 import tilt from "./Assets/js/tilt";
 
 const sourcedata = [{ img: green }, { img: red }, { img: blue }];
