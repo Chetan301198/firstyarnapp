3b6813097a6fe2f53b79b0d14252420033f6cdd3
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-01-04 14:25:03 +0530

    change tilt js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./Components/Cards/Cards";
 import green from "../src/Assets/images/nike1.png";
 import red from "../src/Assets/images/nike2.png";
 import blue from "../src/Assets/images/nike3.png";
-import VanillaTilt from "./Assets/js/VanillaTilt";
+import script from "react-script-tag";
 
 const sourcedata = [{ img: green }, { img: red }, { img: blue }];
 
@@ -15,7 +15,7 @@ function App() {
           return <Card img={val.img} />;
         })}
       </div>
-      <script src={VanillaTilt}></script>
+      <script type="text/javascript" src="../src/Assets/js/tilt.js"></script>
     </>
   );
 }
