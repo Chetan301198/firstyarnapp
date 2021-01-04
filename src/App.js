044af6d737f5b86c8bb928bb8cd4af992735d87c
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-01-04 14:36:18 +0530

    app js file changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Card from "./Components/Cards/Cards";
 import green from "../src/Assets/images/nike1.png";
 import red from "../src/Assets/images/nike2.png";
 import blue from "../src/Assets/images/nike3.png";
-import script from "react-script-tag";
 
 const sourcedata = [{ img: green }, { img: red }, { img: blue }];
 
