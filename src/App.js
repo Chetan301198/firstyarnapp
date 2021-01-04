9e207a3854560a16ff24009324e8418291d6435f
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-01-04 13:53:14 +0530

    changes again app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./Components/Cards/Cards";
 import green from "../src/Assets/images/nike1.png";
 import red from "../src/Assets/images/nike2.png";
 import blue from "../src/Assets/images/nike3.png";
-import tilt from "./Assets/js/tilt";
+import VanillaTilt from "./Assets/js/VanillaTilt";
 
 const sourcedata = [{ img: green }, { img: red }, { img: blue }];
 
@@ -15,7 +15,7 @@ function App() {
           return <Card img={val.img} />;
         })}
       </div>
-      <script src={tilt}></script>
+      <script src={VanillaTilt}></script>
     </>
   );
 }
