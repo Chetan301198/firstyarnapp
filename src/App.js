463fa828220cb6438a295ac63c3c3af4af3690af
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-03-19 17:35:01 +0530

    remove tilt js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Card from "./Components/Cards/Cards";
 import green from "../src/Assets/images/nike1.png";
 import red from "../src/Assets/images/nike2.png";
 import blue from "../src/Assets/images/nike3.png";
-import ScriptTag from "/react-script-tag/lib/index";
 
 const sourcedata = [{ img: green }, { img: red }, { img: blue }];
 
@@ -15,7 +14,6 @@ function App(props) {
           return <Card img={val.img} />;
         })}
       </div>
-      <ScriptTag type="text/javascript" src="/src/Assets/js/tilt.js" />
     </>
   );
 }
