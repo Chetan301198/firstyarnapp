53922ce0f6df3c73b11af22d60d981d91e993e21
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-06-05 12:53:45 +0530

    Fix: parallax effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import "./App.css";
 import Card from "./Components/Cards/Cards";
-import green from "../src/Assets/images/nike1.png";
-import red from "../src/Assets/images/nike2.png";
-import blue from "../src/Assets/images/nike3.png";
+import greenImg from "../src/Assets/images/nike1.png";
+import redImg from "../src/Assets/images/nike2.png";
+import blueImg from "../src/Assets/images/nike3.png";
 
-const sourcedata = [{ img: green }, { img: red }, { img: blue }];
+const sourcedata = [
+  {
+    img: redImg,
+    color: "#fa393a",
+  },
+  {
+    img: greenImg,
+    color: "#9bdc28",
+  },
+  {
+    img: blueImg,
+    color: "#397bf7",
+  },
+];
 
 function App(props) {
   return (
     <>
       <div className="container">
         {sourcedata.map((val) => {
-          return <Card img={val.img} />;
+          return <Card img={val.img} color={val.color} />;
         })}
       </div>
     </>
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,16 +1,36 @@
 import React from "react";
 import "../style.css";
+import Tilt from "react-parallax-tilt";
 
 const Card = (props) => {
   return (
-    <div className="box">
-      <h2 className="name">Air Max 270</h2>
-      <img className="product" src={props.img} alt="Nike Shoes" />
-      <div className="circle"></div>
-      <a href="#" className="buy">
-        Buy Now
-      </a>
-    </div>
+    <>
+      <Tilt
+        className="parallax-effect box"
+        transitionSpeed={400}
+        perspective={1000}
+      >
+        <div className="inner-element">
+          <div>
+            <h2 className="name">Air Max 270</h2>
+            <img className="product" src={props.img} alt="Nike Shoes" />
+            <div
+              className="circle"
+              style={{ backgroundColor: `${props.color}` }}
+            ></div>
+            <a
+              href="#"
+              className="buy"
+              style={{
+                backgroundColor: `${props.color}`,
+              }}
+            >
+              Buy Now
+            </a>
+          </div>
+        </div>
+      </Tilt>
+    </>
   );
 };
 
