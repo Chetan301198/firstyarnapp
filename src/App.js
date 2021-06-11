a0208057737cf9d346b6d3a6744edd2b69250471
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-06-11 20:54:26 +0530

    Change: card page link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import ParallaxCard from "./pages/ParallaxCard";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/card" component={ParallaxCard} />
+          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/" component={ParallaxCard} />
         </Switch>
       </BrowserRouter>
     </>
